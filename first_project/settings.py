e648d6061cf37756cd1e18f236d9e59701c4a5b4
Brian Eddow <brian.eddow@gmail.com>
2016-08-13 15:15:11 -0700

    removed unnecessary settings

--- first_project/settings.py
+++ first_project/settings.py
@@ -120,20 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-CSRF_COOKIE_SECURE = True
-
-CSRF_COOKIE_HTTPONLY = True
-
-X_FRAME_OPTIONS = 'DENY'
-
-SECURE_CONTENT_TYPE_NOSNIFF = True
-
-SECURE_BROWSER_XSS_FILTER = True
-
-SECURE_SSL_REDIRECT = True
-
-SESSION_COOKIE_SECURE = True
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
