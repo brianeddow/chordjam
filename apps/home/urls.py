7e2db67456fbb29b0ba0a30c9bab8afb38be31fe
Brian Eddow <brian.eddow@gmail.com>
2016-08-22 19:24:36 -0700

    edit urls

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,11 +5,11 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
-	url(r'^favicon.ico/$', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False), name="favicon"),
+	url(r'^favicon.ico$', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False), name="favicon"),
 	url(r'^show/(?P<chord_id>\w+)/$', views.show, name='show'),
-	url(r'^chords/$', views.chords, name='chords'),
-	url(r'^review_us/$', views.review_us, name='review_us'),
-	url(r'^submit_review/$', views.SubmitReview.as_view(), name='submit_review'),
-	url(r'^reviews/$', views.reviews, name='reviews'),
-	url(r'^discovery/$', views.discovery, name='discovery')
+	url(r'^chords$', views.chords, name='chords'),
+	url(r'^review_us$', views.review_us, name='review_us'),
+	url(r'^submit_review$', views.SubmitReview.as_view(), name='submit_review'),
+	url(r'^reviews$', views.reviews, name='reviews'),
+	url(r'^discovery$', views.discovery, name='discovery')
 )
