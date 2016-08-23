ac78e13940b53a36423bd02ab9a1babac7be1305
Brian Eddow <brian.eddow@gmail.com>
2016-08-23 16:23:42 -0700

    typo fix

--- first_project/settings.py
+++ first_project/settings.py
@@ -159,5 +159,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # added for heroku
-STATICFILES_STORAGE = 'was whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join((BASE_DIR), 'static')
