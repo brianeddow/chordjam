5132522110848bec2ff727c5f2936cb477a7194f
Brian Eddow <brian.eddow@gmail.com>
2016-08-29 13:02:16 -0700

    updated template dirs

--- first_project/settings.py
+++ first_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,7 +143,6 @@ LOGGING = {
 
 import logging.config
 logging.config.dictConfig(LOGGING)
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
